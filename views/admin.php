48f1aa17a151e3c734a413fc03fd7fc1a906e048
Gabriel Sanchez <gabriel.sanchez17@upr.edu>
2024-02-19 13:10:05 -0400

    modal y 2 forms
    ola :)

--- views/admin.php
+++ views/admin.php
@@ -10,7 +10,9 @@ setData();
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Normateca</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link rel="stylesheet" href="../assets/css/admin.css" />
+
 </head>
 
 <body>
@@ -140,7 +142,7 @@ setData();
           </div>
         </div>
 
-
+        
         <div id="editar" class="editar" style="display: none">
           <div class="backline">
             <h3>Editar Documento</h3>
@@ -149,7 +151,35 @@ setData();
               <input type="text" placeholder="Buscar por nombre" />
               <button type="submit">Buscar</button>
             </div>
-
+            <div class="modal fade bd-example-modal-lg" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form id="formEditarDocumento">
+                            <input type="hidden" id="documentoId" name="documentoId">
+                            <div class="form-group">
+                                <label for="nombreDocumento">Nombre del Documento</label>
+                                <input type="text" class="form-control" id="nombreDocumento" name="nombreDocumento">
+                            </div>
+                            <div class="form-group">
+                                <label for="fechaDocumento">Fecha del Documento</label>
+                                <input type="text" class="form-control" id="fechaDocumento" name="fechaDocumento">
+                            </div>
+                            <!-- Puedes añadir más campos de edición según sea necesario -->
+                        </form>      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
             <table>
               <thead>
                 <tr>
@@ -160,14 +190,19 @@ setData();
               </thead>
               <tbody>
               <?php
-              if (count($_SESSION['documentos']) > 0) {
-                foreach ($_SESSION['documentos'] as $documentos) {
-                  echo '<tr><td>' . $documentos['Document_title'] . '</td><td>' . $documentos['Date_created'] . '</td></tr>';
-                }
-              } else {
-                print '<tr><td colspan="2" style="text-align:center">Documentos no disponibles</td></tr>';
-              }
-              ?>
+        // Suponiendo que $_SESSION['documentos'] contiene los datos de tus documentos
+        if (count($_SESSION['documentos']) > 0) {
+            foreach ($_SESSION['documentos'] as $indice => $documentos) {
+                echo '<tr><td>' . $documentos['Document_title'] . '</td><td>' . $documentos['Date_created'] . '</td>';
+                // Agregar un botón de editar que llame a una función JavaScript para mostrar el modal
+                echo '<td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                    Editar        </button></td></tr>';
+            }
+        } else {
+            // Si no hay documentos disponibles, mostrar un mensaje
+            echo '<tr><td colspan="3" style="text-align:center">Documentos no disponibles</td></tr>';
+        }
+        ?>
               </tbody>
             </table>
 
@@ -347,6 +382,9 @@ setData();
   </footer>
 
   <script src="../assets/js/main.js"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
